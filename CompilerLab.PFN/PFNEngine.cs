a79a0f437f4038ae36cc9c22d583ab5ba0dc5168
AMuqsit <abdemuqsit@hotmail.com>
2017-02-08 10:59:35 +0500

    Add project files.

---
+++ CompilerLab.PFN/PFNEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilerLab.PFN
+{
+    public class PFNEngine
+    {
+        public static string ConvertToPNF(string expression, Dictionary<char, int> precedence)
+        {
+            Stack<char> operandStack = new Stack<char>();
+            StringBuilder pnfString = new StringBuilder();
+
+            var expressionArray = expression.ToCharArray();
+            for (int i = 0; i < expressionArray.Count(); i++)
+            {
+                if (Char.IsDigit(expressionArray[i]))
+                {
+                    pnfString.Append(expressionArray[i]);
+                }
+            }
+            return pnfString.ToString();
+        }
+    }
+}
